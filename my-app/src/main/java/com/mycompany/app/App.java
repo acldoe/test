2c99c98c010b7cb90822d21287fe767b55d0f259
acldoe <35255339+acldoe@users.noreply.github.com>
2019-01-10 16:36:56 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        try{
+            int k = 0;
+        } catch(Exception e) {
+        }
         System.out.println( "Hello World!" );
     }
 }
